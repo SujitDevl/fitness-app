41ed3fbf4aedf91353081414f9cacd18848e4d59
Sujit <pandasujit687@gmail.com>
2025-05-26 23:37:02 +0530

    exercises name

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -25,6 +25,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         alt="dumbbell"
         style={{ width: "40px", height: "40px" }}
       />
+      <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item} </Typography>
     </Stack>
   );
 };
