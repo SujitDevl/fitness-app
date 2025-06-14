d7c527a5dd6ab87c0b72e39b4008ab8332bd0fa7
Sujit <pandasujit687@gmail.com>
2025-06-15 01:06:05 +0530

    scrollbar

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -32,10 +32,10 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
     {data.map((item) => (
       <Box
         key={item.id || item}
-        itemId={item.id || item} // Needed by ScrollMenu
+        itemId={item.id || item} 
         title={item.id || item}
         m="0 40px"
-        component="div" // Ensures it's rendered as div
+        component="div" 
       >
         {bodyParts ? (
           <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
